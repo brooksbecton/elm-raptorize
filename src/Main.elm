79376100e1e3b849b3c891f05e469920fc0d095d
Brooks Becton <bbecton@babelstreet.com>
2018-11-18 12:28:26 -0600

    Migrate from Browser sandbox to BRowser Element

--- src/Main.elm
+++ src/Main.elm
@@ -7,33 +7,46 @@ import Html.Styled.Events exposing (onClick)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view >> toUnstyled }
+  Browser.element { 
+  init = init
+  , update = update
+  , view = view >> toUnstyled
+  , subscriptions = subscriptions }
  
 
 -- MODEL
 
 type alias Model = {isRaptoring: Bool, x: Float, y: Float}
 
-init : Model
-init =
-  {isRaptoring = False, x = 0, y = 0}
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model False 0 0 
+    , Cmd.none
+    )
 
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 -- UPDATE
 
 type Msg = StartRaptoring | StopRaptoring | MoveRaptorLeft Float
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     StartRaptoring ->
-      {model | isRaptoring = True}
+      ({model | isRaptoring = True}
+      , Cmd.none)
     StopRaptoring ->
-      {model | isRaptoring = False}
+      ({model | isRaptoring = False}
+      , Cmd.none)
     MoveRaptorLeft newPos ->
-      {model | x = newPos}
+      ({model | x = newPos}
+      , Cmd.none)
 
--- VIEW
 
 theme : { secondary : Color, primary : Color }
 theme =
@@ -41,6 +54,7 @@ theme =
     , secondary = rgb 250 240 230
     }
 
+-- VIEW
 
 view : Model -> Html Msg
 view model =
@@ -50,5 +64,5 @@ view model =
     , button [ onClick StartRaptoring, css [ backgroundColor theme.primary ]] [ text "Start Raptoring" ]
     , button [ onClick StopRaptoring ] [ text "Stop Raptoring" ]
     , button [ onClick (MoveRaptorLeft (model.x + 120 ))] [ text "Move" ]
-    , img [src "assets/raptor.png", css [position absolute, right (px model.x), bottom (px 0)]] []
+    , img [src "https://raw.githubusercontent.com/brooksbecton/elm-raptorize/master/assets/raptor.png", css [position absolute, right (px model.x), bottom (px 0)]] []
   ]
